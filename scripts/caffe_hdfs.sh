2233910f8c8adf6689f9450011df6eb807eda187
Lionel Gueguen <lgueguen@uber.com>
2016-09-15 11:03:05 -0600

    correct mistak

--- scripts/caffe_hdfs.sh
+++ scripts/caffe_hdfs.sh
@@ -8,7 +8,7 @@ export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export LD_LIBRARY_PATH=$JAVA_HOME/lib/amd64/server/:$LD_LIBRARY_PATH
 
 # Setup HDFS
-export HADOOP_PREFIX := $(dirname $(dirname $(which hdfs)))
+export HADOOP_PREFIX= $(dirname $(dirname $(which hdfs)))
 for jar in `find $HADOOP_PREFIX/share/hadoop/hdfs/ -name \*jar`; do export CLASSPATH=$CLASSPATH:$jar; done
 for jar in `find $HADOOP_PREFIX/share/hadoop/common/ -name \*jar`; do export CLASSPATH=$CLASSPATH:$jar; done
 export LD_LIBRARY_PATH=$HADOOP_PREFIX/lib/native:$LD_LIBRARY_PATH
